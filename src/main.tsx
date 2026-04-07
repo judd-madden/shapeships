8c91f91eccc984dfaf8feb0e26246ce0dd65a66b
Judd Madden <judd@calicoagency.com.au>
2026-04-07 19:06:13 +1000

    Fix flash of unstyled content on app load

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+import { flushSync } from "react-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element #root was not found");
+}
+
+const root = createRoot(rootElement);
+
+flushSync(() => {
+  root.render(<App />);
+});
+
+document.documentElement.classList.add("app-ready");
